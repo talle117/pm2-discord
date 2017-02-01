5a512715006cf106dea66cc4a24c96f59e3ff339
franciscog <frankjg@gmail.com>
2017-01-31 22:48:32 -0500

    initial conversion to discord

--- README.md
+++ README.md
@@ -1,68 +1,67 @@
 # pm2-discord
-I just forked this repo with the plans of converting this to be used with Discord instead of slack. I have not started yet so please ignore everything below for now.
 
-# ~pm2-slack~
-
-This is a PM2 Module for sending events & logs from your PM2 processes to Slack.
+This is a PM2 Module for sending events & logs from your PM2 processes to Discord.
 
 ## Install
 
-To install and setup pm2-slack, run the following commands:
+To install and setup pm2-discord, run the following commands:
 
 ```
-pm2 install pm2-slack
-pm2 set pm2-slack:slack_url https://slack_url
+pm2 install pm2-discord
+pm2 set pm2-discord:discord_url https://[discord_url]
 ```
 
-To get the Slack URL, you need to setup an Incoming Webhook. More details on how to set this up can be found here: https://api.slack.com/incoming-webhooks
+#### `discord_url`
+To get the Discord URL, you need to setup a Webhook. More details on how to set this up can be found here: https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
 
 ## Configure
 
 The following events can be subscribed to:
 
-- log - All standard out logs from your processes. Default: false
-- error - All error logs from your processes. Default: true
-- kill - Event fired when PM2 is killed. Default: true
-- exception - Any exceptions from your processes. Default: true
-- restart - Event fired when a process is restarted. Default: false
-- delete - Event fired when a process is removed from PM2. Default: false
-- stop - Event fired when a process is stopped. Default: false
-- restart overlimit - Event fired when a process is reaches the max amount of times it can restart. Default: true
-- exit - Event fired when a process is exited. Default: false
-- start -  Event fired when a process is started. Default: false
-- online - Event fired when a process is online. Default: false
+- **log** - All standard out logs from your processes. Default: true
+- **error** - All error logs from your processes. Default: true
+- **kill** - Event fired when PM2 is killed. Default: true
+- **exception** - Any exceptions from your processes. Default: true
+- **restart** - Event fired when a process is restarted. Default: false
+- **delete** - Event fired when a process is removed from PM2. Default: false
+- **stop** - Event fired when a process is stopped. Default: true
+- **restart overlimit** - Event fired when a process is reaches the max amount of times it can restart. Default: true
+- **exit** - Event fired when a process is exited. Default: false
+- **start** -  Event fired when a process is started. Default: false
+- **online** - Event fired when a process is online. Default: false
 
 You can simply turn these on and off by setting them to true or false using the PM2 set command.
 
 ```
-pm2 set pm2-slack:log true
-pm2 set pm2-slack:error false
+pm2 set pm2-discord:log true
+pm2 set pm2-discord:error false
 ```
 
 ## Options
 
 The following options are available:
 
-- buffer (bool) - Enable/Disable buffering of messages by timestamp. Messages that occur with the same timestamp (seconds) will be concatenated together and posted as a single slack message. Default: true
+- buffer (bool) - Enable/Disable buffering of messages by timestamp. Messages that occur with the same timestamp (seconds) will be concatenated together and posted as a single discord message. Default: true
 - buffer_seconds (int) - Duration in seconds to aggregate messages. Has no effect if buffer is set to false.  Min: 1, Max: 5, Default: 1
-- queue_max (int) - Number of messages to keep queued before the queue will be truncated. When the queue exceeds this maximum, a rate limit message will be posted to slack. Min: 10, Max: 100, Default: 100
+- queue_max (int) - Number of messages to keep queued before the queue will be truncated. When the queue exceeds this maximum, a rate limit message will be posted to discord. Min: 10, Max: 100, Default: 100
 
 Set these options in the same way you subscribe to events.
 
-Example: The following configuration options will enable message buffering, and set the buffer duration to 2 seconds.  All messages that occur within 2 seconds of each other (for the same event) will be concatenated into a single slack message.
+Example: The following configuration options will enable message buffering, and set the buffer duration to 2 seconds.  All messages that occur within 2 seconds of each other (for the same event) will be concatenated into a single discord message.
 
 ```
-pm2 set pm2-slack:buffer true
-pm2 set pm2-slack:buffer_seconds 2
+pm2 set pm2-discord:buffer true
+pm2 set pm2-discord:buffer_seconds 2
 ```
 
 ## Contributing
 
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
 
+## Acknowledgements
+
+Forked from [mattpker/pm2-slack](https://github.com/mattpker/pm2-slack) and converted to use with Discord. Thanks for the doing all the heavy lifting Matt :stuck_out_tongue_winking_eye:
+
 ## Release History
 
-- 0.3.0 Switched to a default buffer system that groups alike messages by timestamp in the same message to Slack (thanks @kjhangiani)
-- 0.2.0 Implemented a rate limiting system and updated all the dependencies
-- 0.1.1 Commenting & Clean up
 - 0.1.0 Initial Release
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 'use strict';
-
-var os = require('os');
 var pm2 = require('pm2');
 var pmx = require('pmx');
 var request = require('request');
@@ -15,126 +13,111 @@ conf.buffer_seconds = (conf.buffer_seconds > 0 && conf.buffer_seconds < 5) ? con
 // queue max can be between 10 and 100
 conf.queue_max = (conf.queue_max > 10 && conf.queue_max <= 100) ? conf.queue_max : 100;
 
-// Set the events that will trigger the color red
-var redEvents = ['stop', 'exit', 'delete', 'error', 'kill', 'exception', 'restart overlimit', 'suppressed'];
-
 // create the message queue
 var messages = [];
 
 // create the suppressed object for sending suppression messages
 var suppressed = {
-    isSuppressed: false,
-    date: new Date().getTime()
+  isSuppressed: false,
+  date: new Date().getTime()
 };
 
 
-// Function to send event to Slack's Incoming Webhook
-function sendSlack(message) {
+// Function to send event to Discord's Incoming Webhook
+function sendToDiscord(message) {
 
-    var name = message.name;
-    var event = message.event;
-    var description = message.description;
+  var description = message.description;
 
-    // If a Slack URL is not set, we do not want to continue and nofify the user that it needs to be set
-    if (!conf.slack_url) return console.error("There is no Slack URL set, please set the Slack URL: 'pm2 set pm2-slack:slack_url https://slack_url'");
+  // If a Discord URL is not set, we do not want to continue and nofify the user that it needs to be set
+  if (!conf.discord_url) {
+    return console.error("There is no Discord URL set, please set the Discord URL: 'pm2 set pm2-discord:discord_url https://[discord_url]'");
+  }
 
-    // The default color for events should be green
-    var color = '#008E00';
-    // If the event is listed in redEvents, set the color to red
-    if (redEvents.indexOf(event) > -1) {
-        color = '#D00000';
+  // The JSON payload to send to the Webhook
+  var payload = {
+    "content" : description
+  };
+
+  // Options for the post request
+  var options = {
+    method: 'post',
+    body: payload,
+    json: true,
+    url: conf.discord_url
+  };
+
+  // Finally, make the post request to the Discord Incoming Webhook
+  request(options, function(err, res, body) {
+    if (err) {
+      return console.error(err);
     }
-
-    // The JSON payload to send to the Webhook
-    var payload = {
-        username: os.hostname(),
-        attachments: [{
-            fallback: name + ' - ' + event + ' - ' + description,
-            color: color,
-            fields: [{
-                title: name + ' - ' + event,
-                value: description,
-                short: true
-            }]
-        }]
-    };
-
-    // Options for the post request
-    var options = {
-        method: 'post',
-        body: payload,
-        json: true,
-        url: conf.slack_url
-    };
-
-    // Finally, make the post request to the Slack Incoming Webhook
-    request(options, function(err, res, body) {
-        if (err) return console.error(err);
-        if (body !== 'ok') {
-            console.error('Error sending notification to Slack, verify that the Slack URL for incoming webhooks is correct.');
-        }
-    });
+    
+    if (body !== 'ok') {
+      console.error('Error sending notification to Discord, verify that the Discord URL for incoming webhooks is correct.');
+    }
+  });
 }
 
 // Function to get the next buffer of messages (buffer length = 1s)
 function bufferMessage() {
-    var nextMessage = messages.shift();
+  var nextMessage = messages.shift();
 
-    if (!conf.buffer) { return nextMessage; }
+  if (!conf.buffer) { return nextMessage; }
 
-    nextMessage.buffer = [nextMessage.description];
+  nextMessage.buffer = [nextMessage.description];
 
-    // continue shifting elements off the queue while they are the same event and timestamp so they can be buffered together into a single request
-    while (messages.length
-        && (messages[0].timestamp >= nextMessage.timestamp && messages[0].timestamp < (nextMessage.timestamp + conf.buffer_seconds))
-        && messages[0].event === nextMessage.event) {
+  // continue shifting elements off the queue while they are the same event and 
+  // timestamp so they can be buffered together into a single request
+  while (messages.length && 
+    (messages[0].timestamp >= nextMessage.timestamp && 
+      messages[0].timestamp < (nextMessage.timestamp + conf.buffer_seconds)) && 
+    messages[0].event === nextMessage.event) {
 
-        // append description to our buffer and shift the message off the queue and discard it
-        nextMessage.buffer.push(messages[0].description);
-        messages.shift();
+    // append description to our buffer and shift the message off the queue and discard it
+    nextMessage.buffer.push(messages[0].description);
+    messages.shift();
+  }
 
-    }
+  // join the buffer with newlines
+  nextMessage.description = nextMessage.buffer.join("\n");
 
-    // join the buffer with newlines
-    nextMessage.description = nextMessage.buffer.join("\n");
+  // delete the buffer from memory
+  delete nextMessage.buffer;
 
-    // delete the buffer from memory
-    delete nextMessage.buffer;
-
-    return nextMessage;
+  return nextMessage;
 }
 
 // Function to process the message queue
 function processQueue() {
 
-    // If we have a message in the message queue, removed it from the queue and send it to slack
-    if (messages.length > 0) {
-        sendSlack(bufferMessage());
-    }
+  // If we have a message in the message queue, removed it from the queue and send it to discord
+  if (messages.length > 0) {
+    sendToDiscord(bufferMessage());
+  }
 
-    // If there are over conf.queue_max messages in the queue, send the suppression message if it has not been sent and delete all the messages in the queue after this amount (default: 100)
-    if (messages.length > conf.queue_max) {
-        if (!suppressed.isSuppressed) {
-            suppressed.isSuppressed = true;
-            suppressed.date = new Date().getTime();
-            sendSlack({
-                name: 'pm2-slack',
-                event: 'suppressed',
-                description: 'Messages are being suppressed due to rate limiting.'
-            });
-        }
-        messages.splice(conf.queue_max, messages.length);
+  // If there are over conf.queue_max messages in the queue, send the suppression message if it has not been sent and delete all the messages in the queue after this amount (default: 100)
+  if (messages.length > conf.queue_max) {
+    if (!suppressed.isSuppressed) {
+      suppressed.isSuppressed = true;
+      suppressed.date = new Date().getTime();
+      sendToDiscord({
+          name: 'pm2-discord',
+          event: 'suppressed',
+          description: 'Messages are being suppressed due to rate limiting.'
+      });
     }
+    messages.splice(conf.queue_max, messages.length);
+  }
 
-    // If the suppression message has been sent over 1 minute ago, we need to reset it back to false
-    if (suppressed.isSuppressed && suppressed.date < (new Date().getTime() - 60000)) {
-            suppressed.isSuppressed = false;
-    }
+  // If the suppression message has been sent over 1 minute ago, we need to reset it back to false
+  if (suppressed.isSuppressed && suppressed.date < (new Date().getTime() - 60000)) {
+    suppressed.isSuppressed = false;
+  }
 
-    // Wait 10 seconds and then process the next message in the queue
-    setTimeout(function() {
-        processQueue();
-    }, 10000);
+  // Wait 10 seconds and then process the next message in the queue
+  setTimeout(function() {
+    processQueue();
+  }, 10000);
 }
 
 // Start listening on the PM2 BUS
@@ -142,70 +125,70 @@ pm2.launchBus(function(err, bus) {
 
     // Listen for process logs
     if (conf.log) {
-        bus.on('log:out', function(data) {
-            if (data.process.name !== 'pm2-slack') {
-                messages.push({
-                    name: data.process.name,
-                    event: 'log',
-                    description: JSON.stringify(data.data),
-                    timestamp: Math.floor(Date.now() / 1000),
-                });
-            }
-        });
+      bus.on('log:out', function(data) {
+        if (data.process.name !== 'pm2-discord') {
+          messages.push({
+            name: data.process.name,
+            event: 'log',
+            description: JSON.stringify(data.data),
+            timestamp: Math.floor(Date.now() / 1000),
+          });
+        }
+      });
     }
 
     // Listen for process errors
     if (conf.error) {
-        bus.on('log:err', function(data) {
-            if (data.process.name !== 'pm2-slack') {
-                messages.push({
-                    name: data.process.name,
-                    event: 'error',
-                    description: JSON.stringify(data.data),
-                    timestamp: Math.floor(Date.now() / 1000),
-                });
-            }
-        });
+      bus.on('log:err', function(data) {
+        if (data.process.name !== 'pm2-discord') {
+          messages.push({
+            name: data.process.name,
+            event: 'error',
+            description: JSON.stringify(data.data),
+            timestamp: Math.floor(Date.now() / 1000),
+          });
+        }
+      });
     }
 
     // Listen for PM2 kill
     if (conf.kill) {
-        bus.on('pm2:kill', function(data) {
-            messages.push({
-                name: 'PM2',
-                event: 'kill',
-                description: data.msg,
-                timestamp: Math.floor(Date.now() / 1000),
-            });
+      bus.on('pm2:kill', function(data) {
+        messages.push({
+          name: 'PM2',
+          event: 'kill',
+          description: data.msg,
+          timestamp: Math.floor(Date.now() / 1000),
         });
+      });
     }
 
     // Listen for process exceptions
     if (conf.exception) {
-        bus.on('process:exception', function(data) {
-            if (data.process.name !== 'pm2-slack') {
-                messages.push({
-                    name: data.process.name,
-                    event: 'exception',
-                    description: JSON.stringify(data.data),
-                    timestamp: Math.floor(Date.now() / 1000),
-                });
-            }
-        });
+      bus.on('process:exception', function(data) {
+        if (data.process.name !== 'pm2-discord') {
+          messages.push({
+            name: data.process.name,
+            event: 'exception',
+            description: JSON.stringify(data.data),
+            timestamp: Math.floor(Date.now() / 1000),
+          });
+        }
+      });
     }
 
     // Listen for PM2 events
     bus.on('process:event', function(data) {
-        if (conf[data.event]) {
-            if (data.process.name !== 'pm2-slack') {
-                messages.push({
-                    name: data.process.name,
-                    event: data.event,
-                    description: 'The following event has occured on the PM2 process ' + data.process.name + ': ' + data.event,
-                    timestamp: Math.floor(Date.now() / 1000),
-                });
-            }
+      if (conf[data.event]) {
+        if (data.process.name !== 'pm2-discord') {
+          messages.push({
+            name: data.process.name,
+            event: data.event,
+            description: 'The following event has occured on the PM2 process ' + data.process.name + ': ' + data.event,
+            timestamp: Math.floor(Date.now() / 1000),
+          });
         }
+      }
     });
 
     // Start the message processing
